1a0b1abc2450528022207a89650d80dd82aafa7d
Zahi Touqan <touqanzahi@gmail.com>
2024-01-28 16:12:55 +0200

    .

--- src/main/java/com/i3hub/auditingclient/TopicProducer.java
+++ src/main/java/com/i3hub/auditingclient/TopicProducer.java
@@ -1,7 +1,6 @@
 package com.i3hub.auditingclient;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
