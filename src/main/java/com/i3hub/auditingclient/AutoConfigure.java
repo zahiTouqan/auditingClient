3477e81bcaae6c31a27008d6512c5018352c05b3
Zahi Touqan <touqanzahi@gmail.com>
2024-01-28 16:20:31 +0200

    Update AutoConfigure.java

--- src/main/java/com/i3hub/auditingclient/AutoConfigure.java
+++ src/main/java/com/i3hub/auditingclient/AutoConfigure.java
@@ -1,9 +1,7 @@
 package com.i3hub.auditingclient;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableAutoConfiguration
 @AutoConfiguration
 @ComponentScan
 public class AutoConfigure {
